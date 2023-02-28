61a87c8f0bdad04b8adb76b0e0a1bd5257cb061f
irvana <irvan.ahadi22@gmail.com>
2023-02-28 15:39:18 +0700

    remove circleci

--- pkg/app/handler/simobi_handler.go
+++ pkg/app/handler/simobi_handler.go
@@ -2,10 +2,6 @@ package handler
 
 import "github.com/gin-gonic/gin"
 
-// SIMOBI_VERIFY_USER  = ONBOARDING + "/simobiplus/verifyuser"
-// SIMOBI_VERIFY_PHONE = ONBOARDING + "/simobiplus/verifyphone"
-// SIMOBI_OTP          = ONBOARDING + "/simobiplus/otp"
-// SIMOBI_REGISTER     = ONBOARDING + "/simobiplus/register"
 func simobiVerifyUser(ctx *gin.Context) {
 
 }
